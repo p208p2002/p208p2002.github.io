484864c9bd8b552e5a1f0cd1349f5e4d1239c47c
p208p2002 <p208p2002@users.noreply.github.com>
2020-06-20 02:21:00 +0000

    deploy: d7724a5ac77e0307865f1d5f9fa198318a555a4b

---
+++ precache-manifest.503ce0d1f13b99f2f81136a24c093b18.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a08fe5bf25b5b01a488337b054e2a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a05a7aaa4fb281d45a",
+    "url": "/static/css/main.1430288b.chunk.css"
+  },
+  {
+    "revision": "544f6042bff515cadae2",
+    "url": "/static/js/2.37beffc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.37beffc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a05a7aaa4fb281d45a",
+    "url": "/static/js/main.4680c82a.chunk.js"
+  },
+  {
+    "revision": "29a768c22cd0909331d2",
+    "url": "/static/js/runtime-main.e5a428e6.js"
+  },
+  {
+    "revision": "3785c54fef39a98df46224fdb6c44cd7",
+    "url": "/static/media/DeepLearning.3785c54f.jpg"
+  },
+  {
+    "revision": "693f873a2836f5dd457630b6407cc08a",
+    "url": "/static/media/docker.693f873a.svg"
+  },
+  {
+    "revision": "92d3d50bd4ab1d0dcfab3b2e26c2d63e",
+    "url": "/static/media/github.92d3d50b.svg"
+  },
+  {
+    "revision": "d52287738b55788ea70175d566102073",
+    "url": "/static/media/laravel.d5228773.svg"
+  },
+  {
+    "revision": "f006806312dbf953a01945bc57c25740",
+    "url": "/static/media/pytorch-logo.f0068063.png"
+  },
+  {
+    "revision": "33d46e7cf3159d286d3129c9b5feab74",
+    "url": "/static/media/react.33d46e7c.ico"
+  },
+  {
+    "revision": "88ee9aa84cca91defed1e5309016031a",
+    "url": "/static/media/ubuntu.88ee9aa8.svg"
+  }
+]);
