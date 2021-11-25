085de54f97c50b5282b7e73b5e24b4cc82952e3f
p208p2002 <p208p2002@gmail.com>
2021-11-25 11:06:06 +0800

    Change order

--- src/componments/experienceBlock/index.jsx
+++ src/componments/experienceBlock/index.jsx
@@ -80,6 +80,7 @@ const ImageContainer = styled.div`
 export class Index extends Component {
     render() {
         let { experiences = [] } = this.props
+        experiences = experiences.reverse()
         return (
             <Container>
                 <Line>
